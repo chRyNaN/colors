243801cc27ae8cba656eaf4ea1271cb01f6c1b87
Chris <ckeenan@starry.com>
2020-09-08 13:54:27 -0400

    Bumped version to 0.2.1

--- buildSrc/src/main/java/com.chrynan.colors.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.colors.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.colors"
     const val owner = "chrynan"
     const val repoName = "colors"
-    const val versionName = "0.2.0"
-    const val versionCode = 2
-    const val versionDescription = "Pre-release 0.2.0"
+    const val versionName = "0.2.1"
+    const val versionCode = 3
+    const val versionDescription = "Pre-release 0.2.1"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/colors.git"
     const val androidCompileSdkVersion = 29
