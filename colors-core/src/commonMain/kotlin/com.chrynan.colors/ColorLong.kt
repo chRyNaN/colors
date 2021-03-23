a3e8aa0801c1d7ba759aa56b880a37f2fdbdd5d7
Chris <ckeenan@starry.com>
2021-03-23 11:08:04 +0700

    Updated ColorLong documentation

--- colors-core/src/commonMain/kotlin/com.chrynan.colors/ColorLong.kt
+++ colors-core/src/commonMain/kotlin/com.chrynan.colors/ColorLong.kt
@@ -1,12 +1,29 @@
+@file:Suppress("unused")
+
 package com.chrynan.colors
 
+import com.chrynan.colors.space.ColorModel
+
 /**
- * A typealias to an [Int] that is meant to represent a [Color] value in an [Long] form.
- * This is very similar to a [ColorInt] but allows for more precision.
+ * A typealias to an [Long] that is meant to represent a [Color] value in a [Long] form.
+ * This is very similar to a [ColorInt] but allows for more precision. A common color model is the
+ * [ColorModel.RGB] in which the components represent red, green and blue values.
+ *
  * This is similar to the "ColorLong" native representation on the Android platform.
  *
  * Note that where [ColorInt]s always contain ARGB components, a [ColorLong] always has
  * four components that depend on the Color Space Model. One of these components is always
  * alpha, but the other three components depend on the Color Space Model.
+ *
+ * Note that while alpha is encoded in a color long using a 10 bit integer (thus using a range of
+ * [0...1023]), it is converted to and from  float values when decoding and encoding color longs.
+ *
+ * Note that for compatibility reasons and ease of use, color longs encoding sRGB colors do not use
+ * the same encoding as other color longs. In that case, they are more like [ColorInt]s.
+ *
+ * Note that the [ColorModel.CMYK] is unsupported for [ColorLong]s. The [ColorModel.CMYK] is
+ * typically used for printing.
+ *
+ * @see [Android Color Long Documentation](https://developer.android.com/reference/android/graphics/Color.html#color-longs)
  */
 typealias ColorLong = Long
