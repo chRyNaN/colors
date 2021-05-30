55ce72dd5343901cf0b0d06a455029d7803b4450
Chris <ckeenan@starry.com>
2021-05-29 23:19:33 -0500

    Bumped version to 0.3.1 (8)

--- buildSrc/src/main/java/com.chrynan.colors.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.colors.buildSrc/LibraryConstants.kt
@@ -6,8 +6,8 @@ object LibraryConstants {
     const val group = "com.chrynan.colors"
     const val owner = "chrynan"
     const val repoName = "colors"
-    const val versionName = "0.3.0"
-    const val versionCode = 7
+    const val versionName = "0.3.1"
+    const val versionCode = 8
     const val versionDescription = "Release 0.3.0"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/colors.git"
