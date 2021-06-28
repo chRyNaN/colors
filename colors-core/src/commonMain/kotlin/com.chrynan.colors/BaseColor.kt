ffd076f1de112a3a3f07f94ff14c0d826500cf29
Chris <ckeenan@starry.com>
2021-06-28 17:11:12 -0500

    Moved luminance() default implementation to BaseColor

--- colors-core/src/commonMain/kotlin/com.chrynan.colors/BaseColor.kt
+++ colors-core/src/commonMain/kotlin/com.chrynan.colors/BaseColor.kt
@@ -153,4 +153,34 @@ interface BaseColor : Color,
 
         return BaseULongColor(value = colorInt.toULong() shl 32)
     }
+
+    override fun luminance(): Float {
+        var color: Color = this
+        var colorSpace = color.colorSpace
+
+        // According to the following Wiki documentation, the Y in an XYZ Color Space is the
+        // luminance: https://en.wikipedia.org/wiki/Relative_luminance
+        if (colorSpace.model == ColorModel.XYZ) return ((component2() + 2f) / 4f).coerceIn(
+            0.0f,
+            1.0f
+        )
+
+        if (colorSpace.model !== ColorModel.RGB) {
+            color = color.toRgbaColor()
+            colorSpace = color.colorSpace
+        }
+
+        require(colorSpace.model === ColorModel.RGB) {
+            "The specified color must be encoded in either an RGB or XYZ color space. The supplied color space model is ${colorSpace.model}"
+        }
+
+        val eotf = (colorSpace as RgbColorSpace).eotf
+        val r = eotf(color.component1().toDouble())
+        val g = eotf(color.component2().toDouble())
+        val b = eotf(color.component3().toDouble())
+
+        return ((0.2126 * r) + (0.7152 * g) + (0.0722 * b))
+            .toFloat()
+            .coerceIn(0.0f, 1.0f)
+    }
 }
