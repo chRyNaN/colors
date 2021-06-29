9fc5b6c95d2cae5e971d404c73c3c269fdc544e2
Chris <ckeenan@starry.com>
2021-06-28 20:50:33 -0500

    Created AndroidColorUtils

---
+++ colors-core/src/androidMain/kotlin/com/chrynan/colors/AndroidColorUtils.kt
@@ -0,0 +1,45 @@
+@file:Suppress("unused")
+
+package com.chrynan.colors
+
+import android.content.Context
+import android.content.res.Resources
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+/**
+ * Retrieves a [Color] from the provided resource [id] and [theme] using the provided [context].
+ */
+@ExperimentalUnsignedTypes
+fun Color.Companion.fromResource(
+    context: Context,
+    id: Int,
+    theme: Resources.Theme? = null
+): Color {
+    val colorInt = context.resources.getColor(id, theme)
+
+    return Color(colorInt = ColorInt(colorInt))
+}
+
+/**
+ * Retrieves a [ColorInt] from the provided resource [id] and [theme] using the provided [context].
+ */
+@ExperimentalUnsignedTypes
+fun ColorInt.Companion.fromResource(
+    context: Context,
+    id: Int,
+    theme: Resources.Theme? = null
+): ColorInt {
+    val colorInt = context.resources.getColor(id, theme)
+
+    return ColorInt(value = colorInt)
+}
+
+@ExperimentalUnsignedTypes
+@RequiresApi(Build.VERSION_CODES.O)
+fun android.graphics.Color.toMultiplatformColor(): Color {
+    return Color(toArgb()) // TODO should probably use the components and colorspace to retain information and not convert
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun Color.toAndroidColor(): android.graphics.Color = android.graphics.Color.valueOf(colorLong.value)
