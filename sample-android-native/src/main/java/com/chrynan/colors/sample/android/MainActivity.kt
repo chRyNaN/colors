a83fa56f04b7ec8f3095759e038dbdd3bb519a7b
Christopher Keenan <ckeenan@starry.com>
2021-10-20 11:52:25 -0500

    Updated dependencies

--- sample-android-native/src/main/java/com/chrynan/colors/sample/android/MainActivity.kt
+++ sample-android-native/src/main/java/com/chrynan/colors/sample/android/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 import com.chrynan.colors.Color
 import com.chrynan.colors.NamedColor
