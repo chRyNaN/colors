a59ea0a327225a103fcc6888e78ccec6ab065705
Christopher Keenan <ckeenan@starry.com>
2021-12-07 16:46:52 -0600

    Changed Android plugin version to 4.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-beta04")
+        classpath("com.android.tools.build:gradle:4.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.5.30")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.32")
