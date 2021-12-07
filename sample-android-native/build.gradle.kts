b11679203c55fc6af7a4d932704c98e3f553e59e
Christopher Keenan <ckeenan@starry.com>
2021-12-07 16:24:56 -0600

    Updated remaining build.gradle files to use Kotlin

---
+++ sample-android-native/build.gradle.kts
@@ -0,0 +1,68 @@
+import com.chrynan.colors.buildSrc.LibraryConstants
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("org.jetbrains.compose")
+}
+
+group = LibraryConstants.group
+version = LibraryConstants.versionName
+
+android {
+    compileSdk = 31
+    buildToolsVersion = "30.0.3"
+
+    defaultConfig {
+        applicationId = "com.chrynan.colors.sample.android"
+        minSdk = 26
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            // Opt-in to experimental compose APIs
+            freeCompilerArgs = listOf(
+                    "-Xopt-in=kotlin.RequiresOptIn"
+            )
+        }
+    }
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.INHERIT }
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+
+    implementation(compose.runtime)
+    implementation(compose.ui)
+    implementation(compose.material)
+
+    implementation("androidx.compose.compiler:compiler:1.1.0-alpha06")
+    implementation("androidx.compose.ui:ui-tooling:1.1.0-alpha06")
+    implementation("androidx.activity:activity-compose:1.4.0-rc01")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
+    implementation("androidx.compose.material:material-icons-extended:1.1.0-alpha06")
+
+    implementation(project(":colors-compose"))
+    implementation(project(":colors-palette"))
+}
