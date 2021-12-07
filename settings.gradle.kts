81330af6a4e02b8810050554da99db07d6bce99a
Christopher Keenan <ckeenan@starry.com>
2021-12-07 12:21:35 -0600

    Updated settings.gradle file to use Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "colors"
+
+include(":colors-core")
+include(":colors-extension")
+include(":colors-theme")
+include(":colors-palette")
+include(":colors-serialization")
+include(":colors-compose")
+include(":sample-android-native")
