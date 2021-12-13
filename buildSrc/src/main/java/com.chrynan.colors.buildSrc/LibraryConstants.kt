34b0f7538488a72f000cd98f6bd03945fc8982a3
Christopher Keenan <ckeenan@starry.com>
2021-12-12 20:24:24 -0600

    Bumped version to 0.5.2 (20)

--- buildSrc/src/main/java/com.chrynan.colors.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.colors.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.colors"
     const val owner = "chrynan"
     const val repoName = "colors"
-    const val versionName = "0.5.1"
-    const val versionCode = 19
-    const val versionDescription = "Release 0.5.1 (19)"
+    const val versionName = "0.5.2"
+    const val versionCode = 20
+    const val versionDescription = "Release 0.5.2 (20)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/colors.git"
 
