b9855fa09de6c4d938202fc075c28b2b3da27c77
Christopher Keenan <ckeenan@starry.com>
2022-08-20 14:10:38 -0500

    Updated Kotlin to version 1.7.0 and compose-jb to version 1.2.0-alpha01-dev755

--- colors-serialization/build.gradle.kts
+++ colors-serialization/build.gradle.kts
@@ -36,7 +36,7 @@ kotlin {
                 implementation(project(":colors-palette"))
 
                 // Kotlinx Serialization
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
             }
         }
         val iosMain by sourceSets.getting
